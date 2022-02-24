9bbd5b253227e35fc9c6152c61447ae409698c11
David M. Fajardo <davidmf99@gmail.com>
2022-02-24 15:21:04 +0100

    tailwindcss!

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,18 @@
-import type { Component } from 'solid-js';
+import type { Component } from 'solid-js'
 
-import logo from './logo.svg';
-import styles from './App.module.css';
+import logo from './logo.svg'
 
 const App: Component = () => {
-  return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
-  );
-};
+    return (
+        <div class='text-center bg-zinc-900 text-white'>
+            <header class='h-screen flex flex-col justify-center items-center'>
+                <img src={logo} class='w-96 animate-pulse' alt='logo' />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+            </header>
+        </div>
+    )
+}
 
-export default App;
+export default App
